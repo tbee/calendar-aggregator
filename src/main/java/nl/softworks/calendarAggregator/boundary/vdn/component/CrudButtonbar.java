2bcc480e101e43e765a2b7f5174e69c2d1f2ea9e
Tom Eugelink <tbee@tbee.org>
2023-11-10 17:01:08 +0100

    hide buttons if unmapped

--- src/main/java/nl/softworks/calendarAggregator/boundary/vdn/component/CrudButtonbar.java
+++ src/main/java/nl/softworks/calendarAggregator/boundary/vdn/component/CrudButtonbar.java
@@ -9,26 +9,36 @@ public class CrudButtonbar extends HorizontalLayout {
     Runnable onInsert = null;
     Runnable onEdit = null;
     Runnable onDelete = null;
+    Button insertButton = new Button(VaadinIcon.PLUS.create(), e -> onInsert.run());
+    Button editButton = new Button(VaadinIcon.EDIT.create(), e -> onEdit.run());
+    Button deleteButton = new Button(VaadinIcon.TRASH.create(), e -> onDelete.run());
 
     public CrudButtonbar() {
-        Button insertButton = new Button(VaadinIcon.PLUS.create(), e -> onInsert.run());
-        Button editButton = new Button(VaadinIcon.EDIT.create(), e -> onEdit.run());
-        Button deleteButton = new Button(VaadinIcon.TRASH.create(), e -> onDelete.run());
         add(insertButton, editButton, deleteButton);
+        setState();
+    }
+
+    private void setState() {
+        insertButton.setVisible(onInsert != null);
+        editButton.setVisible(onEdit != null);
+        deleteButton.setVisible(onDelete != null);
     }
 
     public CrudButtonbar onInsert(Runnable v) {
         this.onInsert = v;
+        setState();
         return this;
     }
 
     public CrudButtonbar onEdit(Runnable v) {
         this.onEdit = v;
+        setState();
         return this;
     }
 
     public CrudButtonbar onDelete(Runnable v) {
         this.onDelete = v;
+        setState();
         return this;
     }
 }
