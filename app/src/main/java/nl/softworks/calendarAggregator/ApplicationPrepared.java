d6b25b123d86a19d52652e4a11bf9c92337df7ea
Tom Eugelink <tbee@tbee.org>
2024-04-12 08:20:08 +0200

    hsqldb startup to event listener

---
+++ app/src/main/java/nl/softworks/calendarAggregator/ApplicationPrepared.java
@@ -0,0 +1,80 @@
+package nl.softworks.calendarAggregator;
+
+import org.apache.commons.io.IOUtils;
+import org.hsqldb.persist.HsqlProperties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.context.event.ApplicationPreparedEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+/**
+ * Must be registered in META-INF/spring.factories like so:
+ * org.springframework.context.ApplicationListener=nl.softworks.calendarAggregator.ApplicationPrepared
+ */
+public class ApplicationPrepared implements ApplicationListener<ApplicationPreparedEvent> {
+    private static final Logger LOG = LoggerFactory.getLogger(ApplicationPrepared.class);
+
+    @Override
+    public void onApplicationEvent(ApplicationPreparedEvent event) {
+        ConfigurableEnvironment environment = event.getApplicationContext().getEnvironment();
+
+        String url = environment.getProperty("spring.datasource.url");
+        String username = environment.getProperty("spring.datasource.username");
+        String password = environment.getProperty("spring.datasource.password");
+        startHsqldbServer(url, username, password);
+    }
+
+    private void startHsqldbServer(String url, String username, String password) {
+
+        // Determine HSQLDB port
+        if (!url.startsWith("jdbc:hsqldb:hsql://")) {
+            LOG.warn("Not starting HSQLDB because URL seems to be of a different RDBMS: " + url);
+            return;
+        }
+        String datasourceUrlSuffix = url.substring(url.lastIndexOf(":") + 1);
+        String portnr = datasourceUrlSuffix.substring(0, datasourceUrlSuffix.indexOf("/"));
+        String dbname = datasourceUrlSuffix.substring(datasourceUrlSuffix.indexOf("/") + 1);
+
+        HsqlProperties props = new HsqlProperties();
+        props.setProperty("server.database.0", "file:hsqldb/" + dbname + ";user=" + username + ";password=" + password);
+        props.setProperty("server.dbname.0", dbname);
+        props.setProperty("server.port", portnr);
+        props.setProperty("server.acl", createHsqldbAclFile());
+        org.hsqldb.Server dbServer = new org.hsqldb.Server();
+        try {
+            dbServer.setProperties(props);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return;
+        }
+        dbServer.start();
+        if (LOG.isInfoEnabled()) LOG.info("HSQLDB started");
+
+        // Handle clean shutdown
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            if (!dbServer.isNotRunning()) {
+                if (LOG.isInfoEnabled()) LOG.info("HSQLDB shutting down");
+                dbServer.shutdown();
+                if (LOG.isInfoEnabled()) LOG.info("HSQLDB shutdown");
+            }
+        }));
+    }
+
+    private String createHsqldbAclFile() {
+        try {
+            File aclFile = File.createTempFile("hsqldb", "acl");
+            aclFile.deleteOnExit();
+            URL serverACLResourceURL = ApplicationPrepared.class.getResource("/hsqldb.acl");
+            IOUtils.copy(serverACLResourceURL, aclFile);
+            if (LOG.isInfoEnabled()) LOG.info("HSQLDB using " + aclFile.getAbsolutePath());
+            return aclFile.getAbsolutePath();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- app/src/main/java/nl/softworks/calendarAggregator/CalendarAggregateApplication.java
+++ app/src/main/java/nl/softworks/calendarAggregator/CalendarAggregateApplication.java
@@ -1,8 +1,6 @@
 package nl.softworks.calendarAggregator;
 
 import nl.softworks.calendarAggregator.application.jpa.RepoBaseImpl;
-import org.apache.commons.io.IOUtils;
-import org.hsqldb.persist.HsqlProperties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -17,8 +15,6 @@ import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
@@ -28,8 +24,6 @@ import java.security.cert.PKIXParameters;
 import java.security.cert.TrustAnchor;
 import java.security.cert.X509Certificate;
 import java.util.Locale;
-import java.util.Map;
-import java.util.Properties;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
@@ -43,11 +37,9 @@ public class CalendarAggregateApplication {
 
 	public static void main(String[] args) {
 		//listCertificates();
-		//printStackTraces();
 
 		Locale.setDefault(new Locale("NL"));
 		System.setProperty("liquibase.secureParsing", "false");
-		startHsqldbServer();
 		SpringApplication.run(CalendarAggregateApplication.class, args);
 	}
 
@@ -77,20 +69,6 @@ public class CalendarAggregateApplication {
 		}
     }
 
-	private static void printStackTraces() {
-		LOG.info("printStackTraces");
-		Map<Thread, StackTraceElement[]> allStackTraces = Thread.getAllStackTraces();
-		for (Thread thread : allStackTraces.keySet()) {
-			StackTraceElement[] stackTraceElements = allStackTraces.get(thread);
-			if (stackTraceElements.length > 0) {
-				LOG.info("==========");
-			}
-			for (java.lang.StackTraceElement stackTraceElement : stackTraceElements) {
-				LOG.info("[" + thread.getName() + "] " + stackTraceElement.toString());
-			}
-		}
-	}
-
 	@Bean
 	public LocaleResolver localeResolver() {
 		CookieLocaleResolver localeResolver = new CookieLocaleResolver(); // Resolves the locale and stores it in a cookie stored on the user’s machine. https://lokalise.com/blog/spring-boot-internationalization/
@@ -104,76 +82,5 @@ public class CalendarAggregateApplication {
 		messageSource.setBasename("classpath:messages");
 		messageSource.setDefaultEncoding("UTF-8");
 		return messageSource;
-	}
-
-	static private void startHsqldbServer() {
-		// If already started, bail
-		if (hsqldbStarted.getAndSet(true)) { // Restart patch
-			return;
-		}
-
-		// Determine HSQLDB port
-		Properties applicationProperties = loadApplicationProperties();
-		final String datasourceUrl = applicationProperties.getProperty("spring.datasource.url");
-		if (!datasourceUrl.startsWith("jdbc:hsqldb:hsql://")) {
-			LOG.warn("Not starting HSQLDB because URL seems to be of a different RDBMS: " + datasourceUrl);
-			return;
-		}
-		String datasourceUrlSuffix = datasourceUrl.substring(datasourceUrl.lastIndexOf(":") + 1);
-		String portnr = datasourceUrlSuffix.substring(0, datasourceUrlSuffix.indexOf("/"));
-		String dbname = datasourceUrlSuffix.substring(datasourceUrlSuffix.indexOf("/") + 1);
-		final String username = applicationProperties.getProperty("spring.datasource.username");
-		final String password = applicationProperties.getProperty("spring.datasource.password");
-
-		HsqlProperties props = new HsqlProperties();
-	    props.setProperty("server.database.0", "file:hsqldb/" + dbname + ";user=" + username + ";password=" + password);
-	    props.setProperty("server.dbname.0", dbname);
-		props.setProperty("server.port", portnr);
-		props.setProperty("server.acl", createHsqldbAclFile());
-	    org.hsqldb.Server dbServer = new org.hsqldb.Server();
-	    try {
-	        dbServer.setProperties(props);
-	    }
-		catch (Exception e) {
-	    	throw new RuntimeException(e);
-	    }
-	    dbServer.start();
-		if (LOG.isInfoEnabled()) LOG.info("HSQLDB started");
-
-		// Handle clean shutdown
-		Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-			hsqldbStarted.set(false);
-			if (!dbServer.isNotRunning()) {
-				if (LOG.isInfoEnabled()) LOG.info("HSQLDB shutting down");
-				dbServer.shutdown();
-				if (LOG.isInfoEnabled()) LOG.info("HSQLDB shutdown");
-			}
-		}));
-	}
-
-	private static Properties loadApplicationProperties() {
-		Properties properties = new Properties();
-		try (
-			InputStream inputStream = CalendarAggregateApplication.class.getResourceAsStream("/application.properties");
-		) {
-			properties.load(inputStream);
-		}
-		catch (IOException e) {
-			throw new RuntimeException(e);
-		}
-		return properties;
-	}
-
-	private static String createHsqldbAclFile() {
-        try {
-            File aclFile = File.createTempFile("hsqldb", "acl");
-			aclFile.deleteOnExit();
-			URL serverACLResourceURL = CalendarAggregateApplication.class.getResource("/hsqldb.acl");
-			IOUtils.copy(serverACLResourceURL, aclFile);
-			if (LOG.isInfoEnabled()) LOG.info("HSQLDB using " + aclFile.getAbsolutePath());
-			return aclFile.getAbsolutePath();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
 	}
 }
