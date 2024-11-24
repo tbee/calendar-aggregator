ac839a413bdfe80d2fb3b9d4884ce3dfcef573bc
Tom Eugelink <tbee@tbee.org>
2024-11-24 09:35:13 +0100

    use webstack

--- app/src/main/java/nl/softworks/calendarAggregator/ApplicationPrepared.java
+++ app/src/main/java/nl/softworks/calendarAggregator/ApplicationPrepared.java
@@ -1,16 +1,11 @@
 package nl.softworks.calendarAggregator;
 
-import org.apache.commons.io.IOUtils;
-import org.hsqldb.persist.HsqlProperties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.event.ApplicationPreparedEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.core.env.ConfigurableEnvironment;
-
-import java.io.File;
-import java.io.IOException;
-import java.net.URL;
+import org.tbee.webstack.hsqldb.HsqlEmbedded;
 
 /**
  * Must be registered in META-INF/spring.factories like so:
@@ -26,10 +21,6 @@ public class ApplicationPrepared implements ApplicationListener<ApplicationPrepa
         String url = environment.getProperty("spring.datasource.url");
         String username = environment.getProperty("spring.datasource.username");
         String password = environment.getProperty("spring.datasource.password");
-        startHsqldbServer(url, username, password);
-    }
-
-    private void startHsqldbServer(String url, String username, String password) {
 
         // Determine HSQLDB port
         if (!url.startsWith("jdbc:hsqldb:hsql://")) {
@@ -37,44 +28,15 @@ public class ApplicationPrepared implements ApplicationListener<ApplicationPrepa
             return;
         }
         String datasourceUrlSuffix = url.substring(url.lastIndexOf(":") + 1);
-        String portnr = datasourceUrlSuffix.substring(0, datasourceUrlSuffix.indexOf("/"));
+        int portnr = Integer.parseInt(datasourceUrlSuffix.substring(0, datasourceUrlSuffix.indexOf("/")));
         String dbname = datasourceUrlSuffix.substring(datasourceUrlSuffix.indexOf("/") + 1);
 
-        HsqlProperties props = new HsqlProperties();
-        props.setProperty("server.database.0", "file:hsqldb/" + dbname + ";user=" + username + ";password=" + password);
-        props.setProperty("server.dbname.0", dbname);
-        props.setProperty("server.port", portnr);
-        props.setProperty("server.acl", createHsqldbAclFile());
-        org.hsqldb.Server dbServer = new org.hsqldb.Server();
-        try {
-            dbServer.setProperties(props);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return;
-        }
-        dbServer.start();
-        if (LOGGER.isInfoEnabled()) LOGGER.info("HSQLDB started");
-
-        // Handle clean shutdown
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            if (!dbServer.isNotRunning()) {
-                if (LOGGER.isInfoEnabled()) LOGGER.info("HSQLDB shutting down");
-                dbServer.shutdown();
-                if (LOGGER.isInfoEnabled()) LOGGER.info("HSQLDB shutdown");
-            }
-        }));
-    }
-
-    private String createHsqldbAclFile() {
-        try {
-            File aclFile = File.createTempFile("hsqldb", "acl");
-            aclFile.deleteOnExit();
-            URL serverACLResourceURL = ApplicationPrepared.class.getResource("/hsqldb.acl");
-            IOUtils.copy(serverACLResourceURL, aclFile);
-            if (LOGGER.isInfoEnabled()) LOGGER.info("HSQLDB using " + aclFile.getAbsolutePath());
-            return aclFile.getAbsolutePath();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        // Start HSQLDB
+        new HsqlEmbedded()
+                .port(portnr)
+                .username(username)
+                .password(password)
+                .database(dbname)
+                .start();
     }
 }
