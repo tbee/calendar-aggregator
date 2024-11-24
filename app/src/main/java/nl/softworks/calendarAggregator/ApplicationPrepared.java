67fe9fd95117b241c8458500780a07a408bbc5e4
Tom Eugelink <tbee@tbee.org>
2024-11-24 09:49:58 +0100

    convenience methods

--- app/src/main/java/nl/softworks/calendarAggregator/ApplicationPrepared.java
+++ app/src/main/java/nl/softworks/calendarAggregator/ApplicationPrepared.java
@@ -17,26 +17,16 @@ public class ApplicationPrepared implements ApplicationListener<ApplicationPrepa
     @Override
     public void onApplicationEvent(ApplicationPreparedEvent event) {
         ConfigurableEnvironment environment = event.getApplicationContext().getEnvironment();
-
         String url = environment.getProperty("spring.datasource.url");
         String username = environment.getProperty("spring.datasource.username");
         String password = environment.getProperty("spring.datasource.password");
 
-        // Determine HSQLDB port
-        if (!url.startsWith("jdbc:hsqldb:hsql://")) {
-            LOGGER.warn("Not starting HSQLDB because URL seems to be of a different RDBMS: " + url);
-            return;
-        }
-        String datasourceUrlSuffix = url.substring(url.lastIndexOf(":") + 1);
-        int portnr = Integer.parseInt(datasourceUrlSuffix.substring(0, datasourceUrlSuffix.indexOf("/")));
-        String dbname = datasourceUrlSuffix.substring(datasourceUrlSuffix.indexOf("/") + 1);
-
         // Start HSQLDB
         new HsqlEmbedded()
-                .port(portnr)
+                .portFromUrl(url)
                 .username(username)
                 .password(password)
-                .database(dbname)
+                .databaseFromUrl(url)
                 .start();
     }
 }
