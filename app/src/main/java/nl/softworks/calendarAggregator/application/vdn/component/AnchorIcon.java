52e81294b08f9230beb7318859cad5506a23b0a0
Tom Eugelink <tbee@tbee.org>
2025-04-02 07:20:34 +0200

    refactor

--- app/src/main/java/nl/softworks/calendarAggregator/application/vdn/component/AnchorIcon.java
+++ app/src/main/java/nl/softworks/calendarAggregator/application/vdn/component/AnchorIcon.java
@@ -1,9 +1,9 @@
 package nl.softworks.calendarAggregator.application.vdn.component;
 
 import com.vaadin.flow.component.html.Anchor;
-import org.tbee.webstack.vdn.component.fontawesome.FASAnchorIcon;
+import org.tbee.webstack.vdn.component.fontawesome.FasAnchorIcon;
 
-public class AnchorIcon extends FASAnchorIcon {
+public class AnchorIcon extends FasAnchorIcon {
     public AnchorIcon(String href, String... classNames) {
         super(href, classNames);
     }
@@ -12,10 +12,10 @@ public class AnchorIcon extends FASAnchorIcon {
     // Predefined icons
 
     static public Anchor jumpOut(String href) {
-        return href == null ? null : new FASAnchorIcon(href, "fas", "fa-arrow-up-right-from-square");
+        return href == null ? null : new FasAnchorIcon(href, "fas", "fa-arrow-up-right-from-square");
     }
 
     static public Anchor mapPin(String href) {
-        return href == null ? null : new FASAnchorIcon(href, "fas", "fa-map-pin");
+        return href == null ? null : new FasAnchorIcon(href, "fas", "fa-map-pin");
     }
 }
