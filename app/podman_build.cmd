dc3e264b594d0962d957cfd5e9f240e44c0db9b6
Tom Eugelink <tbee@tbee.org>
2025-08-15 11:20:17 +0200

    trying to connect to an external postgres

--- app/podman_build.cmd
+++ app/podman_build.cmd
@@ -1,2 +1,2 @@
-call podman build --iidfile=containerId.txt --file src\main\container\Containerfile . 
+call podman build --iidfile=podman_containerId.txt --file src\main\container\Containerfile . 
 pause
--- app/podman_run.cmd
+++ app/podman_run.cmd
@@ -1,7 +1,8 @@
 call podman rm dancemoments
 
 @rem call podman run --name dancemoments -p 8080:8080 org.tbee.calendaraggregator:latest
-set /p id=<containerId.txt
+set /p id=<podman_containerId.txt
 call podman run --name dancemoments -p 8080:8080 %id%
+@rem use --rm to automatically remove the container after it exits
 
 pause
