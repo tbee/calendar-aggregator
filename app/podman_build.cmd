f0b9b4c26d31573b9fee5f3831e1acbd11f0feb1
Tom Eugelink <tbee@tbee.org>
2025-08-16 11:41:11 +0200

    use tag

--- app/podman_build.cmd
+++ app/podman_build.cmd
@@ -1,2 +1,12 @@
 call podman build --iidfile=podman_containerId.txt --file src\main\container\Containerfile . 
+
+@rem fetch image id of new container
+set /p id=<podman_containerId.txt
+
+call podman untag org.tbee.calendaraggregator:latest 
+
+call podman tag %id% org.tbee.calendaraggregator:latest 
+
+call podman image prune --force 
+
 pause
--- app/podman_run.cmd
+++ app/podman_run.cmd
@@ -1,9 +1,9 @@
 call podman stop dancemoments
 call podman rm dancemoments
 
-@rem call podman run --name dancemoments -p 8080:8080 org.tbee.calendaraggregator:latest
-set /p id=<podman_containerId.txt
-call podman run --name dancemoments -p 8080:8080 %id%
+call podman run --name dancemoments -p 8080:8080 org.tbee.calendaraggregator:latest
+@rem set /p id=<podman_containerId.txt
+@rem call podman run --name dancemoments -p 8080:8080 %id%
 @rem use --rm to automatically remove the container after it exits
 
 pause
