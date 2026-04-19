4ef7b02a7f2f387c5dc20f0e97bd94a21570f6e0
Tom Eugelink <tbee@tbee.org>
2026-04-19 09:22:57 +0200

    start container under linux and docker

--- app/src/test/java/nl/softworks/calendarAggregator/CalendarAggregatorApplicationPostgres.java
+++ app/src/test/java/nl/softworks/calendarAggregator/CalendarAggregatorApplicationPostgres.java
@@ -6,34 +6,25 @@ import java.io.IOException;
 
 public class CalendarAggregatorApplicationPostgres {
 
+    private static PostgreSQLContainer<?> POSTGRES = new PostgreSQLContainer<>("postgres:18")
+            .withDatabaseName("postgres")
+            .withUsername("postgres")
+            .withPassword("password");
+
     public static void main(String[] args) throws Exception {
-        if (!"restartedMain".equals(Thread.currentThread().getName())) {
+        if (!POSTGRES.isRunning()) {
             startPostgres();
         }
         CalendarAggregateApplication.main(args);
     }
 
     private static void startPostgres() throws IOException, InterruptedException {
-        System.out.println("""
-                1. Ensure Podman Desktop is Running.
-                2. Enable Docker API in Podman Desktop:
-                   - Open Podman Desktop
-                   - Go to Settings > Docker Compatibility
-                   - Ensure "Enable Docker API" is checked (it should be on by default)
-                3. Set Environment Variables, either on your system or in the run configuration:
-                   - DOCKER_HOST = "npipe:////./pipe/docker_engine"
-                   - TESTCONTAINERS_RYUK_DISABLED = "true"
-                """);
         // Disable HSQLDB
         System.setProperty("calendaraggregator.hsqldb.start", "false");
 
         // Start postgres container
-        PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:18")
-            .withDatabaseName("postgres")
-            .withUsername("postgres")
-            .withPassword("password");
-        postgres.start();
-        System.out.println("Postgres container started on " + postgres.getJdbcUrl());
+        POSTGRES.start();
+        System.out.println("Postgres container started on " + POSTGRES.getJdbcUrl());
 
 //        // restore database
 //        postgres.copyFileToContainer(
@@ -43,9 +34,9 @@ public class CalendarAggregatorApplicationPostgres {
 //        postgres.execInContainer("psql", "-U", "postgres", "-d", "postgres", "-f", "/tmp/dump.sql");
 
         // Setup springboot
-        System.setProperty("spring.datasource.url", postgres.getJdbcUrl());
-        System.setProperty("spring.datasource.username", postgres.getUsername());
-        System.setProperty("spring.datasource.password", postgres.getPassword());
+        System.setProperty("spring.datasource.url", POSTGRES.getJdbcUrl());
+        System.setProperty("spring.datasource.username", POSTGRES.getUsername());
+        System.setProperty("spring.datasource.password", POSTGRES.getPassword());
         System.setProperty("spring.profiles.active", "dev");
 
         // Start applicatiion
